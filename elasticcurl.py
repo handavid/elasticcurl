bad90f21421a9c9104cbb5823501bc9a2882ccea
Michael Moffitt <Michael.Moffitt@bazaarvoice.com>
2014-01-31 17:58:04 -0600

    Parses first 'limit' lines of input and writes to a temp file. Baby steps...

--- elasticcurl.py
+++ elasticcurl.py
@@ -2,16 +2,24 @@
 
 import sys
 import argparse
+import re
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--limit',  default=100)
-parser.add_argument('--offset', default=0)
-parser.add_argument('--debug',  default=False)
-parser.add_argument('--delete', default=False)
-parser.add_argument('--all',    default=False)
-parser.add_argument('--bulk',   default=False)
-parser.add_argument('--input',  default='http://127.0.0.1:9200/sourceIndex')
-parser.add_argument('--output', default='http://127.0.0.1:9200/destinationIndex')
+parser.add_argument('--input',  required=True)
+parser.add_argument('--output', required=True)
+parser.add_argument('--limit',  type=int,  default=100)
+parser.add_argument('--offset', type=int,  default=0)
+parser.add_argument('--debug',  type=bool, default=False)
 args = parser.parse_args()
 
-print args
+with open(args.input) as inputfile:
+  with open("temp.json",'w') as outputfile:
+    for num in range(0, args.limit):
+      line = inputfile.readline()
+      match = re.search(r'"_index":"([^"]*)","_type":"([^"]*)","_id":"([^"]*)"', line);
+      _index = match.group(1)
+      _type  = match.group(2)
+      _id    = match.group(3)
+      outputfile.write("{ \"index\" : { \"_index\" : \"" + _index + "\", \"_type\" : \"" + _type + "\", \"_id\" : \"" + _id + "\" } }\n")
+      outputfile.write(line)
+
