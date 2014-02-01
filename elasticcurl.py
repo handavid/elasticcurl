899e00fc923e7a5a8d329cd18648e7f3d2c6c521
Michael Moffitt <Michael.Moffitt@bazaarvoice.com>
2014-01-31 18:23:37 -0600

    Basic file-to-es mode seems to work. Bulk API is assumed.

--- elasticcurl.py
+++ elasticcurl.py
@@ -3,23 +3,35 @@
 import sys
 import argparse
 import re
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--input',  required=True)
 parser.add_argument('--output', required=True)
-parser.add_argument('--limit',  type=int,  default=100)
+parser.add_argument('--limit',  type=int,  default=10000)
 parser.add_argument('--offset', type=int,  default=0)
 parser.add_argument('--debug',  type=bool, default=False)
 args = parser.parse_args()
 
 with open(args.input) as inputfile:
-  with open("temp.json",'w') as outputfile:
-    for num in range(0, args.limit):
-      line = inputfile.readline()
-      match = re.search(r'"_index":"([^"]*)","_type":"([^"]*)","_id":"([^"]*)"', line);
-      _index = match.group(1)
-      _type  = match.group(2)
-      _id    = match.group(3)
-      outputfile.write("{ \"index\" : { \"_index\" : \"" + _index + "\", \"_type\" : \"" + _type + "\", \"_id\" : \"" + _id + "\" } }\n")
-      outputfile.write(line)
+  totalLines = 0
+  while True:
+    print "Wrote " + str(totalLines) + " lines"
+    with open("temp.json",'w') as outputfile:
+      for num in range(0, args.limit):
+        line = inputfile.readline()
+        if line == "":
+          sys.exit(0)
+        if line.startswith(","):
+          line = line[1:]
+        totalLines += 1
+        match = re.search(r'"_index":"([^"]*)","_type":"([^"]*)","_id":"([^"]*)"', line);
+        if match == None:
+          continue
+        _index = match.group(1)
+        _type  = match.group(2)
+        _id    = match.group(3)
+        outputfile.write("{ \"index\" : { \"_index\" : \"" + _index + "\", \"_type\" : \"" + _type + "\", \"_id\" : \"" + _id + "\" } }\n")
+        outputfile.write(line)
+    os.system("curl -s -XPOST " + args.output + "/_bulk --data-binary @temp.json > /dev/null")
 
