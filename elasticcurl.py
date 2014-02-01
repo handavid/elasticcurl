2d0144d1c8657ae10c0f5e62dcd7061f1550f042
Michael Moffitt <Michael.Moffitt@bazaarvoice.com>
2014-01-31 18:39:17 -0600

    Minor refactoring

--- elasticcurl.py
+++ elasticcurl.py
@@ -9,29 +9,35 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--input',  required=True)
 parser.add_argument('--output', required=True)
 parser.add_argument('--limit',  type=int,  default=10000)
-parser.add_argument('--offset', type=int,  default=0)
-parser.add_argument('--debug',  type=bool, default=False)
 args = parser.parse_args()
 
-with open(args.input) as inputfile:
-  totalLines = 0
-  while True:
-    print "Wrote " + str(totalLines) + " lines"
-    with open("temp.json",'w') as outputfile:
-      for num in range(0, args.limit):
-        line = inputfile.readline()
-        if line == "":
-          sys.exit(0)
-        if line.startswith(","):
-          line = line[1:]
-        totalLines += 1
-        match = re.search(r'"_index":"([^"]*)","_type":"([^"]*)","_id":"([^"]*)"', line);
-        if match == None:
-          continue
-        _index = match.group(1)
-        _type  = match.group(2)
-        _id    = match.group(3)
-        outputfile.write("{ \"index\" : { \"_index\" : \"" + _index + "\", \"_type\" : \"" + _type + "\", \"_id\" : \"" + _id + "\" } }\n")
-        outputfile.write(line)
-    os.system("curl -s -XPOST " + args.output + "/_bulk --data-binary @temp.json > /dev/null")
+def get_lines_file(limit):
+  global totalLines
+  global inputfile
+  print "Wrote " + str(totalLines) + " lines"
+  with open("/tmp/elasticcurl.json",'w') as outputfile:
+    for num in range(0, limit):
+      line = inputfile.readline()
+      if line == "":
+        return num == 0
+      if line.startswith(","):
+        line = line[1:]
+      totalLines += 1
+      match = re.search(r'"_index":"([^"]*)","_type":"([^"]*)","_id":"([^"]*)"', line);
+      if match == None:
+        continue
+      _index = match.group(1)
+      _type  = match.group(2)
+      _id    = match.group(3)
+      outputfile.write("{ \"index\" : { \"_index\" : \"" + _index + "\", \"_type\" : \"" + _type + "\", \"_id\" : \"" + _id + "\" } }\n")
+      outputfile.write(line)
+  return True
+
+def put_lines_es():
+  os.system("curl -s -XPOST " + args.output + "/_bulk --data-binary @/tmp/elasticcurl.json > /dev/null")
+
+totalLines = 0
+inputfile = open(args.input)
+while (get_lines_file(args.limit)):
+  put_lines_es()
 
